cbea29d85e962a29920c623e09bf6d4f5ec3fa95
Ethan <ethan@unth>
2015-02-19 12:18:34 -0800

    error in error conditions on the client

--- lib/yz/client.rb
+++ lib/yz/client.rb
@@ -1,5 +1,8 @@
 module Yz
   class Client
+    class Error < Yz::Error
+    end
+
     def initialize(options = {})
       # stringify symbol keys
       @options = options.map { |k,v| {k.is_a?(Symbol) ? k.to_s : k => v} }.inject({}, &:update)
@@ -9,13 +12,15 @@ module Yz
       request = Request.new(object)
       request.request_strings.each_with_index do |request_s, i|
         flags = i < request.request_strings.size - 1 ? ZMQ::SNDMORE : 0
-        client_socket.send_string(request_s, flags)
+        send_rc = client_socket.send_string(request_s, flags)
+        raise(Client::Error, "client socket failed to send (errno = #{ZMQ::Util.errno})") if send_rc < 0
       end
       reply_strings = []
       more = true
       while more
         reply_message = ZMQ::Message.create || raise(ServerError, "failed to create message (errno = #{ZMQ::Util.errno})")
         recv_rc = client_socket.recvmsg(reply_message)
+        raise(Client::Error, "client socket failed to recv (errno = #{ZMQ::Util.errno})") if recv_rc < 0
         reply_strings << reply_message.copy_out_string
         reply_message.close
         more = client_socket.more_parts?
@@ -34,12 +39,17 @@ module Yz
     def client_socket
       @client_socket ||= begin
         Yz.zmq_context.socket(ZMQ::REQ).tap do |client_socket|
+          raise(Client::Error, "failed to create client socket") unless client_socket
           if @options['server_public_key'] || @options['client_public_key'] || @options['client_private_key']
-            client_socket.setsockopt(ZMQ::CURVE_SERVERKEY, @options['server_public_key'])
-            client_socket.setsockopt(ZMQ::CURVE_PUBLICKEY, @options['client_public_key'])
-            client_socket.setsockopt(ZMQ::CURVE_SECRETKEY, @options['client_private_key'])
+            rc = client_socket.setsockopt(ZMQ::CURVE_SERVERKEY, @options['server_public_key'])
+            raise(Client::Error, "failed to set client socket server_public_key (errno = #{ZMQ::Util.errno})") if rc < 0
+            rc = client_socket.setsockopt(ZMQ::CURVE_PUBLICKEY, @options['client_public_key'])
+            raise(Client::Error, "failed to set client socket client_public_key (errno = #{ZMQ::Util.errno})") if rc < 0
+            rc = client_socket.setsockopt(ZMQ::CURVE_SECRETKEY, @options['client_private_key'])
+            raise(Client::Error, "failed to set client socket client_private_key (errno = #{ZMQ::Util.errno})") if rc < 0
           end
-          client_socket.connect(@options['connect'])
+          connect_rc = client_socket.connect(@options['connect'])
+          raise(Client::Error, "failed to connect client socket to #{@options['connect']} (errno = #{ZMQ::Util.errno})") if connect_rc < 0
         end
       end
     end
